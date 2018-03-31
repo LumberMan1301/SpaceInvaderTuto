8dca03c11dffabc5207b47114242be50c1ed11ea
Mariano <marianomm@rocketmail.com>
2018-03-31 11:45:31 -0600

    Inicio del 5 tutorial, Game Screen y Sprite

--- src/SpaceInvaders/Sprite/SpriteAnimation.java
+++ src/SpaceInvaders/Sprite/SpriteAnimation.java
@@ -5,4 +5,22 @@ package SpaceInvaders.Sprite;
  * Por definicion: es un grafico de computadora que puede ser movido en pantalla y puede manipularse como una sola entidad
  */
 public class SpriteAnimation {
+    /**
+     * variables de la animacion, ubicacion en pantalla y velocidad de animacion
+     */
+    private double xPos;
+    private double yPos;
+    private int animationSpeed;
+
+    /**
+     * Constructor de la clase
+     * @param xPos
+     * @param yPos
+     * @param animationSpeed
+     */
+    public SpriteAnimation(double xPos, double yPos, int animationSpeed) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.animationSpeed = animationSpeed;
+    }
 }
